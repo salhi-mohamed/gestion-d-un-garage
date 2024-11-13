1f886a40f764a2ac1d7aff7e246767109ec7cb8b
Salhi Med Houssem <salhimedhoussem@gmail.com>
2024-11-13 19:57:03 +0100

    Gestion des employes

--- src/main/java/com/mycompany/garage_manager/Garage_Manager.java
+++ src/main/java/com/mycompany/garage_manager/Garage_Manager.java
@@ -14,15 +14,7 @@ public class Garage_Manager {
    public static void main(String[] args) {
 
 
-// Création de la réceptionniste
-       Receptionniste receptionniste = new Receptionniste(0077,"j","m", 5, "d",4,7, "mp");
-       receptionniste.creerEmploye(0, "salah", "aymen", 0, "mourouj", 2000, "laveur");
-              receptionniste.creerEmploye(1, "salah", "aymen", 0, "mourouj", 2000, "mécanicien");
-              receptionniste.creerEmploye(2, "salah", "aymen", 0, "mourouj", 2000, "chef");
-              receptionniste.creerEmploye(3, "salah", "aymen", 0, "mourouj", 2000, "chef");
-
-             // receptionniste.creerEmploye(2, "salah", "aymen", 0, "mourouj", 2000, "mécanicien");
-              receptionniste.afficherTousLesEmployes();
+        
 
    }
           
